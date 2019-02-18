8c8f156f68a14499f5223bce1eda9ceb257a3b1c
alexioannides <alex.ioanndies@yahoo.co.uk>
2019-02-18 14:02:24 +0000

    Updated subtitle

--- pelicanconf.py
+++ pelicanconf.py
@@ -25,7 +25,7 @@ AUTHOR = 'Dr Alex Ioannides'
 SITEURL = 'https://alexioannides.github.io'
 SITENAME = AUTHOR
 SITETITLE = AUTHOR
-SITESUBTITLE = 'machine learning engineer - (data) scientist - reformed quant - habitual coder'
+SITESUBTITLE = 'machine_learning_engineer - (data)scientist - reformed_quant - habitual_coder'
 SITEDESCRIPION = 'Alex Ioannides on data science: data mining, statistics, machine learning, AI, functional programming, R, Python, Scala, Spark, Elasticsearch, AWS, DevOps...'
 
 # to match old WordPress site
